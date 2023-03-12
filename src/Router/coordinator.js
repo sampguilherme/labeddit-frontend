266409e26c1d8e30780a9f6d1c64ab6d47d00d6c
sampguilherme <guilherme.sampaio324@hotmail.com>
2023-03-12 19:43:24 -0300

    Página de login

---
+++ src/Router/coordinator.js
@@ -0,0 +1,11 @@
+export const goToHomePage = (navigate) => {
+    navigate('/home')
+}
+
+export const goToLoginPage = (navigate) => {
+    navigate('/')
+}
+
+export const goToSignupPage = (navigate) => {
+    navigate('/signup')
+}
---
+++ src/pages/signup/Signup.js
@@ -0,0 +1,7 @@
+export const Signup = () => {
+    return(
+        <>
+            <h1>Projeto</h1>
+        </>
+    )
+}
